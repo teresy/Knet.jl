c66818af297f1b627d6123a59803db913a50618e
Deniz Yuret <denizyuret@gmail.com>
2015-12-28 12:11:26 -0800

    lrn.jl: added DivisiveNormalization

--- src/op/lrn.jl
+++ src/op/lrn.jl
@@ -1,11 +1,24 @@
-type LRN <: Op; n; alpha; beta; k; end
-lrn(x,y;n=5,alpha=1e-4,beta=0.75,k=2.0,o...)=(LRN(n,alpha,beta,k),x,y)
+type LRN <: Op; lrnN; lrnAlpha; lrnBeta; lrnK; end
+lrn(x,y;lrnN=5,lrnAlpha=1e-4,lrnBeta=0.75,lrnK=2.0,o...)=(LRN(lrnN,lrnAlpha,lrnBeta,lrnK),x,y)
 ninputs(::LRN)=1
 overwrites(::LRN)=false
 back_reads_x(::LRN)=true
 back_reads_y(::LRN)=true
 infersize(::LRN,xdims,ydims)=infersize(Sigm(),xdims,ydims)
 forw(l::LRN, x, y; o...)=
-    (cudnnLRNCrossChannelForward(x,y; n=l.n, alpha=l.alpha, beta=l.beta, k=l.k); gpusync(); y)
+    (cudnnLRNCrossChannelForward(x,y; lrnN=l.lrnN, lrnAlpha=l.lrnAlpha, lrnBeta=l.lrnBeta, lrnK=l.lrnK); gpusync(); y)
 back(l::LRN, dy, dx; x=nothing, y=nothing, o...)=
-    (dx!=nothing && cudnnLRNCrossChannelBackward(y,dy,x,dx;n=l.n, alpha=l.alpha, beta=l.beta, k=l.k); gpusync(); dx)
+    (dx!=nothing && cudnnLRNCrossChannelBackward(y,dy,x,dx; lrnN=l.lrnN, lrnAlpha=l.lrnAlpha, lrnBeta=l.lrnBeta, lrnK=l.lrnK); gpusync(); dx)
+
+type DivN <: Op; lrnN; lrnAlpha; lrnBeta; lrnK; end
+divn(x,y;lrnN=5,lrnAlpha=1e-4,lrnBeta=0.75,lrnK=2.0,o...)=(DivN(lrnN,lrnAlpha,lrnBeta,lrnK),x,y)
+ninputs(::DivN)=1
+overwrites(::DivN)=false
+back_reads_x(::DivN)=false      # different from LRN
+back_reads_y(::DivN)=true
+infersize(::DivN,xdims,ydims)=infersize(Sigm(),xdims,ydims)
+forw(l::DivN, x, y; o...)=
+    (cudnnDivisiveNormalizationForward(x,y; lrnN=l.lrnN, lrnAlpha=l.lrnAlpha, lrnBeta=l.lrnBeta, lrnK=l.lrnK); gpusync(); y)
+back(l::DivN, dy, dx; y=nothing, o...)=
+    (dx!=nothing && cudnnDivisiveNormalizationBackward(y,dy,dx; lrnN=l.lrnN, lrnAlpha=l.lrnAlpha, lrnBeta=l.lrnBeta, lrnK=l.lrnK); gpusync(); dx) # different from LRN
+
