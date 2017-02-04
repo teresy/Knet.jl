11b12040e91d32bd7b2d23decad2d54a85c4d2d9
Deniz Yuret <denizyuret@gmail.com>
2017-02-04 10:44:49 +0300

    cpuconv wip

--- src/cuda44.jl
+++ src/cuda44.jl
@@ -25,7 +25,7 @@ Here is a description of all available keyword arguments:
 * stride: the number of elements to slide to reach the next filtering window. Default=1.
 * upscale: upscale factor for each dimension. Default=1.
 * mode: 0 for convolution and 1 for cross-correlation.  Default=0.
-* alpha: can be used to scale the result. Default=1.
+* alpha,beta: blend or scale the result: dstValue = alpha*srcValue + beta*priorDstValue. Default=1,0.
 * algo: specifies which convolution algorithm shoud be used to compute the results. Default=0. See the CUDNN User Guide for details.
 * workSpace: data pointer to GPU memory to a workspace needed to able to execute the specified algorithm. Default=C_NULL.
 * workSpaceSizeInBytes: the size in bytes of the provided workSpace. Default=0.
@@ -38,7 +38,7 @@ function conv4{T}(w::KnetArray{T},x::KnetArray{T};
     y = similar(x, cdims(w,x;o...))
     @cuda(cudnn, cudnnConvolutionForward,
           (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,   UInt32,Cptr,     Csize_t,             Ptr{T},Cptr,Ptr{T}),
-          handle,Ref(alpha),TD(x),x,FD(w),w,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(beta),TD(y),y)
+          handle,Ref(T(alpha)),TD(x),x,FD(w),w,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(T(beta)),TD(y),y)
     return y
 end
 
@@ -49,15 +49,15 @@ function conv4x{T}(w::KnetArray{T},x::KnetArray{T},dy::KnetArray{T};
     if cudnnVersion >= 4000
         @cuda(cudnn,cudnnConvolutionBackwardData,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,     UInt32,Cptr,     Csize_t,             Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),FD(w),w,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(beta),TD(dx),dx)
+              handle,Ref(T(alpha)),FD(w),w,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(T(beta)),TD(dx),dx)
     elseif cudnnVersion >= 3000
         @cuda(cudnn,cudnnConvolutionBackwardData_v3,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,     UInt32,Cptr,     Csize_t,             Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),FD(w),w,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(beta),TD(dx),dx)
+              handle,Ref(T(alpha)),FD(w),w,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(T(beta)),TD(dx),dx)
     else
         @cuda(cudnn,cudnnConvolutionBackwardData,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,       Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),FD(w),w,TD(dy),dy,CD(w,x;o...),Ref(beta),TD(dx),dx)
+              handle,Ref(T(alpha)),FD(w),w,TD(dy),dy,CD(w,x;o...),Ref(T(beta)),TD(dx),dx)
     end
     return dx
 end
@@ -69,15 +69,15 @@ function conv4w{T}(w::KnetArray{T},x::KnetArray{T},dy::KnetArray{T};
     if cudnnVersion >= 4000
         @cuda(cudnn,cudnnConvolutionBackwardFilter,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,     UInt32,Cptr,     Csize_t,             Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),TD(x),x,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(beta),FD(dw),dw)
+              handle,Ref(T(alpha)),TD(x),x,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(T(beta)),FD(dw),dw)
     elseif cudnnVersion >= 3000
         @cuda(cudnn,cudnnConvolutionBackwardFilter_v3,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,     UInt32,Cptr,     Csize_t,             Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),TD(x),x,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(beta),FD(dw),dw)
+              handle,Ref(T(alpha)),TD(x),x,TD(dy),dy,CD(w,x;o...),algo,workSpace,workSpaceSizeInBytes,Ref(T(beta)),FD(dw),dw)
     else
         @cuda(cudnn,cudnnConvolutionBackwardFilter,
               (Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,       Ptr{T},Cptr,Ptr{T}),
-              handle,Ref(alpha),TD(x),x,TD(dy),dy,CD(w,x;o...),Ref(beta),FD(dw),dw)
+              handle,Ref(T(alpha)),TD(x),x,TD(dy),dy,CD(w,x;o...),Ref(T(beta)),FD(dw),dw)
     end
     return dw
 end
@@ -121,7 +121,7 @@ function pool{T}(x::KnetArray{T}; handle=cudnnhandle, alpha=one(T), beta=zero(T)
     y = similar(x, pdims(x; o...))
     @cuda(cudnn, cudnnPoolingForward,
           (Cptr, Cptr,      Ptr{T},    Cptr,Ptr{T},Ptr{T},   Cptr,Ptr{T}),
-          handle,PD(x;o...),Ref(alpha),TD(x),x,    Ref(beta),TD(y),y)
+          handle,PD(x;o...),Ref(T(alpha)),TD(x),x,    Ref(T(beta)),TD(y),y)
     return y
 end
 
@@ -130,7 +130,7 @@ function poolx{T}(x::KnetArray{T},y::KnetArray{T},dy::KnetArray{T};
     dx = similar(x)
     @cuda(cudnn,cudnnPoolingBackward,
           (Cptr,Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Cptr,Ptr{T},Ptr{T},Cptr,Ptr{T}),
-          handle,PD(x;o...),Ref(alpha),TD(y),y,TD(dy),dy,TD(x),x,Ref(beta),TD(dx),dx)
+          handle,PD(x;o...),Ref(T(alpha)),TD(y),y,TD(dy),dy,TD(x),x,Ref(T(beta)),TD(dx),dx)
     return dx
 end
 
@@ -248,7 +248,8 @@ DT(::KnetArray{Float32})=UInt32(0)
 DT(::KnetArray{Float64})=UInt32(1)
 DT(::KnetArray{Float16})=UInt32(2)
 
-function cdims{T,N}(w::KnetArray{T,N},x::KnetArray{T,N}; padding=0, stride=1, o...)
+function cdims(w,x; padding=padsize(w), stride=1, o...)
+    N = ndims(w)
     ntuple(N) do i
         if i < N-1
             pi = (if isa(padding,Number); padding; else padding[i]; end)
@@ -262,7 +263,8 @@ function cdims{T,N}(w::KnetArray{T,N},x::KnetArray{T,N}; padding=0, stride=1, o.
     end
 end
 
-function pdims{T,N}(x::KnetArray{T,N}; window=2, padding=0, stride=window, o...)
+function pdims(x; window=2, padding=0, stride=window, o...)
+    N = ndims(x)
     ntuple(N) do i
         if i < N-1
             wi = (if isa(window,Number); window; else window[i]; end)
@@ -299,4 +301,191 @@ function mat(x)
         throw(MethodError(mat,x))
     end
 end
+
+
+### CPU implementations: originally by Onur Kuru, 2016; adapted to Knet8 by Deniz Yuret, 2017
+
+function _conv2{T}(x::Array{T,2}, w::Array{T,2}; padding=(0,0), stride=(1,1), mode=0)
+    pad = Int[0,0]
+    for i=1:2
+        pad[i] = size(w,i)-1-padding[i]
+        if pad[i] < 0; error("cpu conv4 does not support padding >= w"); end
+    end
+    if mode==1; w=rot180(w); end
+    y = conv2(x, w)
+    return y[1+pad[1]:stride[1]:end-pad[1], 1+pad[2]:stride[2]:end-pad[2]]
+end
+
+function conv4{T}(w::Array{T,4},x::Array{T,4}; alpha=1, beta=0,
+                  padding=padsize(w), stride=(1,1), upscale=1, mode=0, # 0=conv, 1=xcorr
+                  o...) # handle=cudnnhandle, algo=0, workSpace=C_NULL, workSpaceSizeInBytes=0
+    if beta != 0; error("cpu conv4 only supports beta=0"); end
+    if upscale != 1; error("cpu conv4 only supports upscale=1"); end
+    if isa(padding,Number); padding=(padding,padding); end
+    if isa(stride,Number); stride=(stride,stride); end
+    # To support stride!=1 we need a weird operation in conv4w which scatters dy into a larger array
+    if stride != (1,1); error("cpu conv4 only supports stride=1"); end
+    y = fill!(similar(x, cdims(w,x; padding=padding, stride=stride)), 0)
+    Wx,Hx,Cx,N = size(x)
+    Ww,Hw,Cw,K = size(w)
+    if !(Cx==Cw && Hx>=Hw && Wx>=Ww); throw(DimensionMismatch()); end
+    @inbounds for n in 1:N, k in 1:K, c in 1:Cx
+        y[:,:,k,n] += _conv2(x[:,:,c,n], w[:,:,c,k]; padding=padding, stride=stride, mode=mode)
+    end
+    if alpha!=1; y = alpha*y; end
+    return y
+end
+
+# dw = rot180(xcorr(x,dy)) if y=conv(w,x)
+# dw = xcorr(x,dy) if y=xcorr(w,x)
+# TODO: stride != 1 not working
+function conv4w{T}(w::Array{T,4}, x::Array{T,4}, dy::Array{T,4}; alpha=1,
+                  padding=padsize(w), stride=(1,1), mode=0, # 0=conv, 1=xcorr
+                  o...) # handle=cudnnhandle, algo=0, workSpace=C_NULL, workSpaceSizeInBytes=0, upscale=1, beta=0
+    Wx,Hx,C,Nx = size(x)
+    Wy,Hy,K,Ny = size(dy)
+    dw = fill!(similar(w),0)
+    @inbounds for c in 1:C, k in 1:K, n in 1:Ny
+        dw1 = _conv2(x[:,:,c,n], dy[:,:,k,n]; padding=padding, stride=stride, mode=1)
+        if mode==0; dw1=rot180(dw1); end
+        dw[:,:,c,k] += dw1
+    end
+    if alpha!=1; dw = alpha*dw; end
+    return dw
+end
+
+# dx = xcorr(dy, w, 'full')
+function conv4x{T}(w::Array{T,4}, x::Array{T,4}, dy::Array{T,4}; alpha=1,
+                   padding=padsize(w), stride=(1,1), mode=0, # 0=conv, 1=xcorr
+                   o...) # handle=cudnnhandle, algo=0, workSpace=C_NULL, workSpaceSizeInBytes=0, upscale=1, beta=0
+    Wy,Hy,Ky,N = size(dy)
+    Ww,Hw,C,Kw = size(w)
+    pad1 = Ww - 1 - padding[1]
+    pad2 = Hw - 1 - padding[2]
+    dx = fill!(similar(x),0)
+    @inbounds for n in 1:N, c in 1:C, k in 1:Kw
+        dx1 = _conv2(dy[:,:,k,n], w[:,:,c,k]; padding=(pad1,pad2), stride=stride, mode=1-mode)
+        dx[:,:,c,n] += dx1
+    end
+    if alpha!=1; dx = alpha*dx; end
+    return dx
+end
+
+function pool{T}(x::Array{T,4}; alpha=1, beta=0,
+                 window=(2,2), stride=window, padding=0, mode=0, # 0:max, 1:avg+pad, 2:avg-pad
+                 maxpoolingNanOpt=0, o...) # handle=cudnnhandle
+    if beta != 0; error("cpu pool only supports beta=0"); end
+    if mode != 0; error("cpu pool only supports mode=0"); end # TODO
+    if padding != 0; error("cpu pool only supports padding=0"); end # TODO
+    if stride != window; error("cpu pool only supports stride=window"); end # TODO
+    if maxpoolingNanOpt != 0; error("cpu pool only supports maxpoolingNanOpt=0"); end # TODO: check what this means
+    if isa(window,Number); window=(window,window); end
+    y = fill!(similar(x,pdims(x;window=window,padding=padding,stride=stride)),0)
+    Wx,Hx,C,Nx = size(x);
+    Wy,Hy,K,Ny = size(y);
+    @inbounds for n in 1:Nx, c in 1:C, i in 0:stride[1]:Wx-stride[1], j in 0:stride[2]:Hx-stride[2]
+        iy, jy = div(i,stride[1])+1, div(j,stride[2])+1
+        wx_end = min(i+window[1], Wx)
+        hx_end = min(j+window[2], Hx)
+        y[iy,jy,c,n] = maximum(x[i+1:wx_end,j+1:hx_end,c,n])
+    end
+    if alpha != 1; y = alpha * y; end
+    return y
+end
+
+function poolx{T}(x::Array{T,4}, y::Array{T,4}, dy::Array{T,4}; alpha=1, beta=0,
+                 window=(2,2), stride=window, padding=0, mode=0, # 0:max, 1:avg+pad, 2:avg-pad
+                 maxpoolingNanOpt=0, o...) # handle=cudnnhandle
+    dx = fill!(similar(x), 0)
+    Wx,Hx,C,Nx = size(x);
+    Wy,Hy,K,Ny = size(y);
+    @inbounds for n in 1:Nx, c in 1:C, i in 0:stride[1]:Wx-stride[1], j in 0:stride[2]:Hx-stride[2]
+        iy, jy = div(i,stride[1])+1, div(j,stride[2])+1
+        wx_end = min(i+window[1], Wx)
+        hx_end = min(j+window[2], Hx)
+        a = x[i+1:wx_end,j+1:hx_end,c,n]
+        di,dj = ind2sub(a,indmax(a))
+        dx[i+di,j+dj,c,n] += dy[iy,jy,c,n]
+    end
+    if alpha!=1; dx = alpha * dx; end
+    return dx
+end
+
+# function cudnnGetConvolutionNdForwardOutputDim{T}(x::Array{T,4}, w::Array{T,4}; padding=padding,stride=stride)
+#     Wx,Hx,Cx,N = size(x)
+#     Ww,Hw,Cw,K = size(w)
+#     @assert Cx==Cw
+#     Wy,Hy = floor(Int, 1 + (Int[Wx,Hx] + 2*padding - Int[Ww,Hw]) / stride)
+#     return (Wy,Hy,K,N)
+# end
+
+# function cudnnGetPoolingNdForwardOutputDim{T}(x::Array{T,4}; window=2, padding=0, stride=1, mode=CUDNN_POOLING_MAX)
+#     # @assert padding==0 && stride==1 && mode==CUDNN_POOLING_MAX
+#     dims = [size(x)...]
+#     # (mode, pdims, window, padding, stride) = cudnnGetPoolingNdDescriptor(pd)
+#     for i=1:length(dims)-2
+#         # dims[i] = 1 + floor((dims[i] + 2*padding - window) / stride)
+#         dims[i] = 1 + ceil((dims[i] + 2*padding - window) / stride)
+#     end
+#     tuple(dims...)
+# end
+
+# if GPU
+#     import CUDNN: cudnnConvolutionForward, cudnnConvolutionBackwardFilter, cudnnConvolutionBackwardData, cudnnPoolingForward, cudnnPoolingBackward
+#     using CUDNN: CUDNN_CONVOLUTION, CUDNN_CROSS_CORRELATION, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM, CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM, CUDNN_CONVOLUTION_FWD_ALGO_GEMM, CUDNN_CONVOLUTION_FWD_ALGO_DIRECT, CUDNN_CONVOLUTION_FWD_ALGO_FFT, CUDNN_POOLING_MAX, CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING, CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING
+# else
+#     const CUDNN_CONVOLUTION = (UInt32)(0)
+#     const CUDNN_CROSS_CORRELATION = (UInt32)(1)
+#     const CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM = (UInt32)(0)
+#     const CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_PRECOMP_GEMM = (UInt32)(1)
+#     const CUDNN_CONVOLUTION_FWD_ALGO_GEMM = (UInt32)(2)
+#     const CUDNN_CONVOLUTION_FWD_ALGO_DIRECT = (UInt32)(3)
+#     const CUDNN_CONVOLUTION_FWD_ALGO_FFT = (UInt32)(4)
+#     const CUDNN_POOLING_MAX = (UInt32)(0)
+#     const CUDNN_POOLING_AVERAGE_COUNT_INCLUDE_PADDING = (UInt32)(1)
+#     const CUDNN_POOLING_AVERAGE_COUNT_EXCLUDE_PADDING = (UInt32)(2)
+# end
+
+# function cudnnConvolutionForward{T}(x::Array{T,4}, w::Array{T,4}, y::Array{T,4}; padding=0, stride=1, 
+#                                     upscale=1, mode=CUDNN_CONVOLUTION, cd=nothing,
+#                                     algorithm=CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM,
+#                                     workSpace=0, workSpaceSizeInBytes=0, alpha=1, beta=1,im2col=1)
+#     # x: (W,H,C,N)
+#     # w: (W,H,C,K) 
+#     # y: (W,H,K,N) 
+#     fill!(y,0)
+#     @assert (padding==0 && stride==1 && upscale==1 && mode==CUDNN_CONVOLUTION && algorithm == CUDNN_CONVOLUTION_FWD_ALGO_IMPLICIT_GEMM) "$((padding,stride,upscale,mode,algorithm))"
+#     Wx,Hx,Cx,N = size(x)
+#     Ww,Hw,Cw,K = size(w)
+#     @assert (Cx==Cw && Hx>=Hw && Wx>=Ww) "$((Wx,Hw,Ww,Hw))"
+
+#     @inbounds for n in 1:N, k in 1:K, c in 1:Cx
+#         y[:,:,k,n] += _conv2_gemm(x[:,:,c,n], w[:,:,c,k]; pad=padding, stride=stride, xcorr=mode!=0)
+#     end
+#     return y
+# end
+
+# # Alternative to _conv2 but 5x slower.
+# # Does not handle tuple padding
+# # Does not handle strides != 1
+# function _conv2_gemm{T}(x0::Array{T,2}, w0::Array{T,2}; padding=0, stride=1, mode=0)
+#     if padding > 0
+#         x=zeros(eltype(x0),map(m->2*padding+m,size(x0))) 
+#         x[padding+1:end-padding,padding+1:end-padding] = x0
+#     else
+#         x=x0
+#     end
+#     if mode==1
+#         w = vec(w0)
+#     else
+#         w = reverse(vec(w0))
+#     end
+#     rwindow, cwindow = size(w0)
+#     row_extend = size(x,1)-rwindow+1
+#     col_extend = size(x,2)-cwindow+1
+#     widx = [(j-1)*size(x,1)+i for i in 1:row_extend, j in 1:col_extend]
+#     oidx = [(j-1)*size(x,1)+i for i in 1:rwindow, j in 1:cwindow]
+#     destidx = [i+(j-1) for i in vec(widx), j in vec(oidx)]
+#     return reshape(x[destidx]*w,row_extend,col_extend)
+# end
 
---
+++ test/cpuconv.jl
@@ -0,0 +1,125 @@
+using Knet, BenchmarkTools
+using Knet: conv4w,_conv2,padsize,conv4x,poolx
+
+function testconv4()
+    for t in (Float32, Float64)
+        cx = rand(t, (9,8,7,6))
+        cw = rand(t, (3,4,7,5))
+        gx = KnetArray(cx)
+        gw = KnetArray(cw)
+        for p in ((0,0), (1,1), (2,2))
+            for s in ((1,1),) #  2) TODO stride!=1
+                for u in (1,)       # u!=1 does not work on gpu or cpu
+                    for m in (0, 1)
+                        for a in (1, 2)
+                            for b in (0,) # b!=0 does not make sense when allocating y
+                                println((t,:p,p,:s,s,:u,u,:m,m,:a,a,:b,b))
+                                cy = conv4(cw,cx; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                gy = conv4(gw,gx; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                tst1 = isapprox(cy, Array(gy))
+                                cdw = conv4w(cw,cx,cy; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                gdw = conv4w(gw,gx,gy; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                tst2 = isapprox(cdw, Array(gdw))
+                                cdx = conv4x(cw,cx,cy; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                gdx = conv4x(gw,gx,gy; padding=p, stride=s, upscale=u, mode=m, alpha=a, beta=b)
+                                tst3 = isapprox(cdx, Array(gdx))
+                                println((tst1,tst2,tst3))
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function timeconv4()
+    for t in (Float32, Float64)
+        println(t)
+        cx = rand(t, (256,256,3,10))
+        cw = rand(t, (5, 5, 3, 20))
+        @time conv4(cw,cx;padding=0)
+        @time conv4(cw,cx;padding=0)
+        @time conv4(cw,cx;padding=0)
+    end
+end
+
+function testpool4()
+    for t in (Float32, Float64)
+        cx = rand(t, (9,8,7,6))
+        gx = KnetArray(cx)
+        for w in ((2,2), (3,3), (2,3))
+            # for p in ((0,0), (1,1), (2,2))
+            p = 0 # TODO
+            # for s in (w, (1,1))
+            s = w # TODO
+            # for m in (0, 1, 2)
+            m = 0 # TODO
+            n = 0 # TODO
+            # for b in (0,) # b!=0 does not make sense when allocating y
+            b = 0
+            # for a in (1, 2) # a!=1 is broken in CUDNN back
+            a = 1
+            println((t,:w,w,:p,p,:s,s,:m,m,:n,n,:a,a,:b,b))
+            cy = pool(cx; window=w, padding=p, stride=s, mode=m, maxpoolingNanOpt=n, alpha=a, beta=b)
+            gy = pool(gx; window=w, padding=p, stride=s, mode=m, maxpoolingNanOpt=n, alpha=a, beta=b)
+            tst1 = isapprox(cy, Array(gy))
+            cdx = poolx(cx,cy,cy; window=w, padding=p, stride=s, mode=m, maxpoolingNanOpt=n, alpha=a, beta=b)
+            gdx = poolx(gx,gy,gy; window=w, padding=p, stride=s, mode=m, maxpoolingNanOpt=n, alpha=a, beta=b)
+            tst2 = isapprox(cdx, Array(gdx))
+            println((tst1,tst2))
+        end
+    end
+end
+
+
+function _conv3{T}(x::Array{T,2}, w::Array{T,2}; padding=(0,0), stride=(1,1), mode=0)
+    pad = Int[0,0]
+    for i=1:2
+        pad[i] = size(w,i)-1-padding[i]
+        if pad[i] < 0; error("cpu conv4 does not support padding >= w"); end
+    end
+    if mode==1; w=rot180(w); end
+    @show y = conv2(x, w)
+    return y[1+pad[1]:stride[1]:end-pad[1], 1+pad[2]:stride[2]:end-pad[2]]
+end
+
+# dx = xcorr(dy, w, 'full')
+function _conv4x{T}(w::Array{T,4}, x::Array{T,4}, dy::Array{T,4}; alpha=1,
+                   padding=padsize(w), stride=(1,1), mode=0, # 0=conv, 1=xcorr
+                   o...) # handle=cudnnhandle, algo=0, workSpace=C_NULL, workSpaceSizeInBytes=0, upscale=1, beta=0
+    Wy,Hy,Ky,N = size(dy)
+    Ww,Hw,C,Kw = size(w)
+    pad1 = Ww - 1 - padding[1]
+    pad2 = Hw - 1 - padding[2]
+    dx = fill!(similar(x),0)
+    @inbounds for n in 1:N, c in 1:C, k in 1:Kw
+        w1 = w[:,:,c,k]
+        # if mode==1; w1=rot180(w1); end
+        dx1 = _conv2(dy[:,:,k,n], w1; padding=(pad1,pad2), stride=stride, mode=1-mode)
+        dx[:,:,c,n] += dx1
+    end
+    if alpha!=1; dx = alpha*dx; end
+    return dx
+end
+
+# o = Dict(:mode=>1,:stride=>(1,1),:padding=>(1,0))
+o = Dict(:mode=>0,:stride=>(1,1),:padding=>(1,0))
+cw = reshape([1.0:3.0...], (3,1,1,1))
+cx = reshape([10.0:10.0:50.0...], (5,1,1,1))
+cy = conv4(cw,cx;o...)
+gw = KnetArray(cw)
+gx = KnetArray(cx)
+gy = conv4(gw,gx;o...)
+cy1 = ones(cy)
+gy1 = KnetArray(cy1)
+cdw = conv4w(cw,cx,cy;o...)
+gdw = conv4w(gw,gx,gy;o...)
+cdx = conv4x(cw,cx,cy;o...)
+gdx = conv4x(gw,gx,gy;o...)
+p = Dict(:window=>(2,1),:alpha=>2)
+cy = pool(cx; p...)
+gy = pool(gx; p...)
+cdx = poolx(cx,cy,cy; p...)
+gdx = poolx(gx,gy,gy; p...)
+isapprox(cdx, Array(gdx))
